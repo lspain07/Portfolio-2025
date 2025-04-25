e1c2475552eedfe698623240902807a985d6fd80
lspain07 <lukasrspain@gmail.com>
2025-04-25 12:21:44 -0500

    Update Rock-Paper-Scissors.py

--- 2025/Rock-Paper-Scissors.py
+++ 2025/Rock-Paper-Scissors.py
@@ -4,27 +4,27 @@ import random
 bot_choice = random.randint(1,3)# 1 = rock, 2 = paper, 3 = scissors
 
 def rock():
-  if bot_choice = 1:
+  if bot_choice == 1:
     label_result.config(text="Rock! Go Again!")
-  if bot_choice = 2:
+  if bot_choice == 2:
     label_result.config(text="Paper! You lose!")
-  if bot_choice = 3:
+  if bot_choice == 3:
     label_result.config(text="Scissors! You win!")
 
 def paper():
-  if bot_choice = 1:
+  if bot_choice == 1:
     label_result.config(text="Rock! You win!")
-  if bot_choice = 2:
+  if bot_choice == 2:
     label_result.config(text="Paper! Go Again!")
-  if bot_choice = 3:
+  if bot_choice == 3:
     label_result.config(text="Scissors! You lose!")
 
 def scissors():
-  if bot_choice = 1:
+  if bot_choice == 1:
     label_result.config(text="Rock! You lose!")
-  if bot_choice = 2:
+  if bot_choice == 2:
     label_result.config(text="Paper! You win!")
-  if bot_choice = 3:
+  if bot_choice == 3:
     label_result.config(text="Scissors! Go again!")
 
 main = tk.Tk()

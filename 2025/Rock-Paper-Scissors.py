65d3c3d382a514ce15d5fec09b59dab782f55bd9
lspain07 <lukasrspain@gmail.com>
2025-05-05 12:20:02 -0500

    Finish Rock-Paper-Scissors.py

--- 2025/Rock-Paper-Scissors.py
+++ 2025/Rock-Paper-Scissors.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import PhotoImage
+from PIL import Image, ImageTk
 import random
 
 # 1 = rock, 2 = paper, 3 = scissors
@@ -32,26 +33,28 @@ def scissors():
     label_result.config(text="Scissors! Go again!")
 
 main = tk.Tk()
-main.title("Guess the Number!")
-main.geometry("960x600")
+main.title("Rock! Paper! Scissors!")
 
 label_guess = tk.Label(main, text="Which one?")
 label_guess.pack()
 
-rock1 = PhotoImage(file="C:\Users\lukspain\rock.png")
-paper1 = PhotoImage(file="C:\Users\lukspain\paper.png")
-scissors1 = PhotoImage(file="C:\Users\lukspain\scissors.png")
-
-button_rock = tk.Button(main, image=rock1, command=rock)
-button_rock.pack()
-
-button_paper = tk.Button(main, image=paper1, command=paper)
-button_paper.pack()
-
-button_scissors = tk.Button(main, image=scissors1, command=scissors)
-button_scissors.pack()
-
 label_result = tk.Label(main, text="")
 label_result.pack()
+
+rock1 = Image.open('rock.png')
+rock1 = ImageTk.PhotoImage(rock1)
+paper1 = Image.open('paper.png')
+paper1 = ImageTk.PhotoImage(paper1)
+scissors1 = Image.open('scissors.png')
+scissors1 = ImageTk.PhotoImage(scissors1)
+
+button_rock = tk.Button(main, image=rock1, command=rock)
+button_rock.pack(side=tk.LEFT)
+
+button_paper = tk.Button(main, image=paper1, command=paper)
+button_paper.pack(side=tk.LEFT)
+
+button_scissors = tk.Button(main, image=scissors1, command=scissors)
+button_scissors.pack(side=tk.LEFT)
 
 main.mainloop()

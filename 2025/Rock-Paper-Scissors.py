9b09e51a79d9acabe1d2607eab285113454454a3
lspain07 <lukasrspain@gmail.com>
2025-05-06 12:39:04 -0500

    Update Rock-Paper-Scissors.py

--- 2025/Rock-Paper-Scissors.py
+++ 2025/Rock-Paper-Scissors.py
@@ -5,32 +5,57 @@ import random
 
 # 1 = rock, 2 = paper, 3 = scissors
 
+win = 0
+loss = 0
+draw = 0
+
 def rock():
   bot_choice1 = random.randint(1,3)
+  global win, loss, draw
   if bot_choice1 == 1:
     label_result.config(text="Rock! Go again!")
+    draw += 1
+    draw_counter.config(text=f"Draws: {draw}")
   if bot_choice1 == 2:
     label_result.config(text="Paper! You lose!")
+    loss += 1
+    loss_counter.config(text=f"Losses: {loss}")
   if bot_choice1 == 3:
     label_result.config(text="Scissors! You win!")
+    win += 1
+    win_counter.config(text=f"Wins: {win}")
 
 def paper():
   bot_choice2 = random.randint(1,3)
+  global win, loss, draw
   if bot_choice2 == 1:
     label_result.config(text="Rock! You win!")
+    win += 1
+    win_counter.config(text=f"Wins: {win}")
   if bot_choice2 == 2:
     label_result.config(text="Paper! Go again!")
+    draw += 1
+    draw_counter.config(text=f"Draws: {draw}")
   if bot_choice2 == 3:
     label_result.config(text="Scissors! You lose!")
+    loss += 1
+    loss_counter.config(text=f"Losses: {loss}")
 
 def scissors():
   bot_choice3 = random.randint(1,3)
+  global win, loss, draw
   if bot_choice3 == 1:
     label_result.config(text="Rock! You lose!")
+    loss += 1
+    loss_counter.config(text=f"Losses: {loss}")
   if bot_choice3 == 2:
     label_result.config(text="Paper! You win!")
+    win += 1
+    win_counter.config(text=f"Wins: {win}")
   if bot_choice3 == 3:
     label_result.config(text="Scissors! Go again!")
+    draw += 1
+    draw_counter.config(text=f"Draws: {draw}")
 
 main = tk.Tk()
 main.title("Rock! Paper! Scissors!")
@@ -40,6 +65,15 @@ label_guess.pack()
 
 label_result = tk.Label(main, text="")
 label_result.pack()
+
+win_counter = tk.Label(main, text=f"Wins: {win}")
+win_counter.pack()
+
+loss_counter = tk.Label(main, text=f"Losses: {loss}")
+loss_counter.pack()
+
+draw_counter = tk.Label(main, text=f"Draws: {draw}")
+draw_counter.pack()
 
 rock1 = Image.open('rock.png')
 rock1 = ImageTk.PhotoImage(rock1)
@@ -58,5 +92,3 @@ button_scissors = tk.Button(main, image=scissors1, command=scissors)
 button_scissors.pack(side=tk.LEFT)
 
 main.mainloop()
-#I need to figure out how to add a counter that tells
-#the user how many wins, losses, and draws they have

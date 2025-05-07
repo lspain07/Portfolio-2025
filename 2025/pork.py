f2a12ea258c552ea2a611b722d6d14035a2148f1
lspain07 <lukasrspain@gmail.com>
2025-05-07 12:09:58 -0500

    Update pork.py

--- 2025/pork.py
+++ 2025/pork.py
@@ -1,1 +1,12 @@
 #this is just me trying to recreate Zork I in python
+
+#Possible mechanics:
+#inventory button that displays what you have picked up
+#places visited viewer
+#toggleable assistance/tutorial
+
+#1 - establish basic game window/GUI
+#2 - write a basic throughline for the player to follow
+#3 - describe end goal and how to achieve it
+#4 - make game mechanics to keep the player entertained cause monkey brain (score system?)
+#5 - 

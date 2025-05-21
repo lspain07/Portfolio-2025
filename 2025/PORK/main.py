77838c8e66cd13d865619294f7a234deefbc2afd
lspain07 <lukasrspain@gmail.com>
2025-05-20 21:14:52 -0500

    Update main.py

--- 2025/PORK/main.py
+++ 2025/PORK/main.py
@@ -1,11 +1,10 @@
 import random
+import sys
 
 items = ["longsword","lantern"]
+lantern_status = False #False = off/True = on
 
-#class #gonna have to learn how to use classes to make all "move" variables applicable under the quit function
-
-#def quit(): #this needs to return the false boolean value if the player ever types "quit"
-#if 
+#class/should probably use classes to simply player commands
 
 def help():
   print("""
@@ -47,8 +46,97 @@ You are standing to the south of the forest in a wide green clearing
 There is a path ripped through the brush
 """)
 
+def forest7():
+  print("")
+
+def forest6():
+  print("")
+
+def forest5():
+  global lantern_status
+  global inventory
+  while True:
+    try:
+      move6 = str(input(""))
+      if move6.capitalize == "N":
+        print("""
+        The brambles still block any further travel along the path
+        """)
+      elif move6 == "longsword" and lantern_status == True:
+        print("""
+        You manage to roughly chop yourself a hole in the wall of thorns after swinging the blade like a machete for a minute
+        The forest is still dark ahead, but your lantern is a good enough light to make your way
+        """)
+        break
+        forest6()
+      elif move6 == "longsword" and lantern_status == False:
+        print("""
+        Swinging wildly at the dark isn't a very good idea, is it?
+        You have no idea what you're cutting into, but eventually you manage to slam your blade into the hide of a tree\n
+        Tugging on the blade reveals it is stuck
+        """)
+        del inventory[0]
+        break
+        forest7()
+      elif move6 =="inventory":
+        inventory()
+      elif move6 == "help":
+        help()
+      elif move6 == "quit":
+        sys.exit("Thanks for playing!")
+      else:
+        print("")
+    except ValueError:
+        print("")
+    except KeyboardInterrupt:
+        print("")
+        
+
+def forest4():
+  while True:
+    try:
+      move5 = str(input(""))
+      if move5 == "longsword":
+        print("""
+        The moment you brandish the blade hanging at your waist, the two eyes reflected before you disappear further back into the black
+        """)
+        break
+        forest5()
+      elif move5 =="inventory":
+        inventory()
+      elif move5 == "help":
+        help()
+      elif move5 == "quit":
+        sys.exit("Thanks for playing!")
+      else:
+        print("")
+    except ValueError:
+        print("")
+    except KeyboardInterrupt:
+        print("")
+
 def forest3():
-    print("")
+  while True:
+    try:
+      move4 = str(input(""))
+      if move4.capitalize() == "S":
+        print("""
+        Apparently, you aren't strong enough to brave the depths of the forest
+        The parents won't be very happy...
+        """)
+        break
+      elif move4 =="inventory":
+        inventory()
+      elif move4 == "help":
+        help()
+      elif move4 == "quit":
+        sys.exit("Thanks for playing!")
+      else:
+        print("")
+    except ValueError:
+        print("")
+    except KeyboardInterrupt:
+        print("")
 
 def forest2():
     while True:
@@ -56,7 +144,7 @@ def forest2():
             move3 = str(input(""))
             if move3.capitalize() == "W":
                 print("""
-                
+                Strafing around your intial entry point yields nothing
                 """)
                 forest3()
                 break
@@ -64,8 +152,10 @@ def forest2():
                 inventory()
             elif move3 == "help":
                 help()
-            elif move3 == false:
-                break
+            elif move3 == "quit":
+                sys.exit("Thanks for playing!")
+            else:
+              print("")
         except ValueError:
             print("")
         except KeyboardInterrupt:
@@ -77,19 +167,21 @@ def forest1():
     try:
         move2 = str(input(""))
         if move2.capitalize() == "S":
-            print("""
-            You retreat from the forest back to the clearing, listening as the rustling continues
-            """)
-            forest2()
-            break
+          print("""
+          You retreat from the forest back to the clearing, listening as the rustling continues
+          """)
+          forest2()
+          break
         elif move2.capitalize() == "W" or "E":
-            print("The forest is too dense to go that way")
+          print("The forest is too dense to go that way")
         elif move2 == "inventory":
           inventory()
         elif move2 == "help":
           help()
-        elif move3 == false:
-          break
+        elif move2 == "quit":
+          sys.exit("Thanks for playing!")
+        else:
+          print("")
     except ValueError:
         print("")
     except KeyboardInterrupt:
@@ -101,8 +193,8 @@ def forest():
       move1 = str(input(""))
       if move1.capitalize() == "N":
         print("""
-        You take your first step into the forest path\n
-        Darkness encroaches upon you, and you hear the rustling of beasts in the bushes around you\n
+        You take your first step into the forest path
+        Darkness encroaches upon you, and you hear the rustling of beasts in the bushes around you
         You can not see anything in the dark, but the rustling is getting closer
         """)
         forest1()
@@ -111,10 +203,23 @@ def forest():
         inventory()
       elif move1 == "help":
         help()
-      elif move3 == false:
+      elif move1 == "lantern":
+        print("""
+        You unclip your lantern from your belt
+        and brandish it at the darkness\n
+        Despite the light, you can only see but a few meters in front of you
+        where the northern path is enveloped by thorns and brambles\n
+        In addition,
+        the light is reflecting off the eyes of something watching you in those same thorns and brambles
+        """)
+        forest4()
         break
-      else:
+      elif move1 == "quit":
+        sys.exit("Thanks for playing!")
+      elif move1.capitalize() == "S" or "E" or "W":
         print("There is nothing for you that way")
+      else:
+        print("")
     except ValueError:
         print("")
     except KeyboardInterrupt:

28e2e30fefce3166cb7fa07278d417a4e121294e
lspain07 <lukasrspain@gmail.com>
2025-08-22 13:01:45 -0500

    Delete rust directory

--- rust/hello-cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\lukas\rust\hello-cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-C:\Users\lukas\rust\hello-cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-src\main.rs:
--- rust/hello-cargo/target/debug/hello-cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\lukas\rust\hello-cargo\target\debug\hello-cargo.exe: C:\Users\lukas\rust\hello-cargo\src\main.rs

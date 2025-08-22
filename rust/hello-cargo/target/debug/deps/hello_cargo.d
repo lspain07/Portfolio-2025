85cecb8e37558dac19c0ed8ceefad3f3a65868cc
lspain07 <lukasrspain@gmail.com>
2025-08-22 12:06:02 -0500

    Add files via upload

---
+++ rust/hello-cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\lukas\rust\hello-cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+C:\Users\lukas\rust\hello-cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+src\main.rs:
---
+++ rust/hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+C:\Users\lukas\rust\hello-cargo\target\debug\hello-cargo.exe: C:\Users\lukas\rust\hello-cargo\src\main.rs
